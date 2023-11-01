e11d3548b5c8888ca5985012fb477d2046421562
Terry (shenkez) <shenkez23@gmail.com>
2023-11-01 06:21:30 -0700

    combination

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(9):
+    for j in range(1, 10):
+        if i == j or j < i:
+            pass
+        elif j == 9 and i == 8:
+            print("{}{},".format((i), (j)))
+        else:
+            print("{}{},".format((i), (j)), end=" ")
