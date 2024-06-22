be9a38f8c67f3adb6951aab62e3c9be1d42143fc
Terry <shenkez23@gmail.com>
2024-06-22 02:08:08 -0700

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,26 +1,8 @@
-// Retrieve arguments from the command line, excluding the first two default ones (node and script path)
-let args = process.argv.slice(2);
-
-// Convert all arguments to integers
-let integers = args.map(arg => parseInt(arg, 10));
-
-// Check if there are less than 2 integers
-if (integers.length < 2) {
-    console.log(0);
-} else {
-    // Find the second largest integer
-    let max = -Infinity;
-    let secondMax = -Infinity;
-
-    for (let num of integers) {
-        if (num > max) {
-            secondMax = max;
-            max = num;
-        } else if (num > secondMax && num < max) {
-            secondMax = num;
-        }
-    }
-
-    console.log(secondMax);
+#!/usr/bin/node
+let nextMax = 0;
+const args = process.argv.slice(2);
+if (args.length > 1) {
+  args.sort();
+  nextMax = args[args.length - 2];
 }
-
+console.log(nextMax);
